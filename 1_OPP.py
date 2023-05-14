c285f128f41359b85cef2d04f83fb0cc7722afce
Patryk_G <pw28358@student.wsb.wroclaw.pl>
2023-05-14 09:48:47 +0200

    2023_05_13_UI_UX_commit_002

--- 1_OPP.py
+++ 1_OPP.py
@@ -1,2 +1,49 @@
 class Auto:
-    def __init__(self, ):
+    def __init__(self, barwa, info_o_kondycji, rok_produkcji):
+        self._kolor = barwa
+        self.ilosc_paliwa = 10
+        self.kondycja = info_o_kondycji
+        self.__spalanie_na_100 = 14
+        self.__tryb_ekonomiczny = False
+        self.wiek = 2023 - rok_produkcji
+        self.przebieg = 0
+
+    def aktualizacja_przebiegu(self, przebieg):
+        if przebieg > 0:
+            self.przebieg += przebieg
+        else:
+            print('zła wartosc')
+
+    def admin_aktualizacja_przebiegu(self, przebieg1, przebieg2):
+        if przebieg1 == przebieg2:
+            self.przebieg += przebieg1
+            print('zmieniles przebieg o ',przebieg1)
+            if przebieg1 < 0:
+                print('Wysylam maila do prezesa')
+
+    def wlacz_eco(self):
+        self.__spalanie_na_100 = 8
+        self.__tryb_ekonomiczny = True
+
+    def wylacz_eco(self):
+        self.__spalanie_na_100 = 14
+        self.__tryb_ekonomiczny = False
+
+    def wyswietl_tryb(self):
+        if self.__spalanie_na_100 > 10:
+            print('spalanie jest nieco wyzsze')
+        else:
+            print('spalanie wynosi TYLKO ',self.__spalanie_na_100)
+
+moje_auto = Auto('czerwony', 3, 1965)
+# print(moje_auto.__kondycja)
+# moje_auto.__kondycja += 1
+# print(moje_auto.__kondycja)
+
+moje_auto.wlacz_eco()
+moje_auto.wyswietl_tryb()
+moje_auto.aktualizacja_przebiegu(20)
+print(moje_auto.przebieg)
+moje_auto.aktualizacja_przebiegu(-50)
+print(moje_auto.przebieg)
+
---
+++ 3_Kivy_label_input_gui.py
@@ -0,0 +1,50 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="Imie: "))
+        self.imie = TextInput(multiline=False)
+        self.inside.add_widget(self.imie)
+
+        self.inside.add_widget(Label(text="Nazwisko: "))
+        self.nazwisko = TextInput(multiline=False)
+        self.inside.add_widget(self.nazwisko)
+
+        self.inside.add_widget(Label(text="rok_urodzenia: "))
+        self.rok_urodzenia = TextInput(multiline=False)
+        self.inside.add_widget(self.rok_urodzenia)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=42)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        print('wcisniety')
+        imie = self.imie.text
+        nazwisko = self.nazwisko.text
+        rok_urodz = self.rok_urodzenia.text
+        print(f'Imię: {imie}, nazwisko: {nazwisko}\nrok urodzenia: {rok_urodz}')
+        self.imie.text = ''
+        self.nazwisko.text = ''
+        self.rok_urodzenia.text = ''
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+if __name__ == "__main__":
+    MyApp().run()
